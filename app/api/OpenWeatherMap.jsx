3586c659d350dbf3d6449acbe370b5eaf4d9744c
Spankied <angel.owner@yahoo.com>
2017-05-26 05:04:02 -0400

    Initial Commit

---
+++ app/api/OpenWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=3a2aec4f8e85d1cbec7f98010874d545&units=imperial';
+
+//3a2aec4f8e85d1cbec7f98010874d545
+
+module.exports =  {
+  getTemp: function (location){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function(res){
+      if(res.data.cod && res.data.message){
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res){
+      throw new Error(res.data.message);
+    });
+  }
+}
