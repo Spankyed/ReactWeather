2ad7b893cea5a782cd33b987215a739709632082
Spankied <angel.owner@yahoo.com>
2017-05-27 06:24:01 -0400

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,25 @@ var About = (props) => {
   return (
     <div>
       <h1 className="text-center">About</h1>
-      <p>This application was created to learn more about the React framework</p>
+      <p>This application was built for learning purposes. </p>
+      <p>Here are some tools used..</p>
+      <ul>
+        <li>
+          <a href='https://nodejs.org'>Nodejs</a> JavaScript runtime
+        </li>
+        <li>
+          <a href='https://expressjs.com/'>Express</a> web application framework
+        </li>
+        <li>Facebook's
+          <a href='https://facebook.github.io/react'> React</a> Framework
+        </li>
+        <li>
+          <a href='https://foundation.zurb.com/'>Foundation</a> front-end framework
+        </li>
+        <li>Open Weather Map
+          <a href='http://openweathermap.org/api'> API</a>
+        </li>
+      </ul>
     </div>
   )
 };
